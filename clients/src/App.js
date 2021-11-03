8fad753af24a1d59250dea3f792db305c243da6d
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-11-04 02:09:16 +0700

    create layout

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,24 +1,47 @@
 import React from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="navbar-test">
+        <div className="logo">Logo</div>
+        <div className="test-title">
+          <div className="title">Test Title</div>
+          <div className="submit-title">submit</div>
+        </div>
+        <div className="settings">
+          <div className="preview-test">preview</div>
+          <div className="save-test">save</div>
+          <div className="exit-test">exit</div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="panel-left">
+          <div className="questions-preview"></div>
+          <div className="question-actions">
+            <button className="add-question">Add question</button>
+          </div>
+        </div>
+        <div className="panel-center">
+          <div className="question-content">Câu hỏi</div>
+          <div className="question-embed">Ảnh minh họa</div>
+          <div className="question-answers">
+            <div className="answer">A</div>
+            <div className="answer">A</div>
+            <div className="answer">A</div>
+            <div className="answer">A</div>
+          </div>
+        </div>
+        <div className="panel-right">
+          <div className="attributes"></div>
+          <div className="quick-actions">
+            <button className="delete-question">Delete</button>
+            <button className="duplicate-question">Duplicate</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
