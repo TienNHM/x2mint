1843c329cd7504f9174870a3ecb529b55ad5e2c6
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-11-20 22:49:20 +0700

    routing

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Navbar from 'components/common/navbar/Navbar'
 import MultiChoices from 'components/multi-choices/multichoices/MultiChoices'
 import Contest from 'components/contestManagement/contest/Contest'
 
+
 function App() {
     return (
-        <div className="app">
-            <div className="bootstrap-container">
-                {/* <Navbar />
-                <MultiChoices /> */}
-                <Contest />
+        <>
+            <div className="app">
+                <div className="bootstrap-container">
+                    <Navbar />
+
+                    <Routes>
+                        <Route exact path="/" element={<Contest />} />
+                        <Route path="/login" element={<Contest />} />
+                        <Route path="/profile" element={<Contest />} />
+                        <Route path="/contest" element={<Contest />} />
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
