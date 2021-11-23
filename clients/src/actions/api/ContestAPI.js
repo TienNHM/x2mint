053f0c31ea6f03341164f4bf1a681967f299cfc5
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-11-23 15:48:45 +0700

    fix Contest API

--- clients/src/actions/api/ContestAPI.js
+++ clients/src/actions/api/ContestAPI.js
@@ -1,49 +1,10 @@
 import axios from 'axios'
-import { auth } from 'actions/api/AuthAPI'
 import { API_ROOT } from 'utils/constants'
 
-const username = 'minhhoang1'
-const password = '12345'
-
-export const fetchAllContests = async () => {
-    var accessToken = await auth(username, password).then(data => data.accessToken)
-
+export const fetchAllContests = async (accessToken) => {
+    //TODO: fetchAllContests
     const url = `${API_ROOT}/app/api/v1/contests`
     const request = await axios.get(url, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        }
-    })
-
-    return request.data
-}
-
-export const getAllContestsByCreator = async (id) => {
-    var accessToken = await auth(username, password).then(data => data.accessToken)
-
-    const url = `${API_ROOT}/app/api/v1/contests/creator/${id}`
-    const request = await axios.get(url, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        }
-    })
-
-    return request.data
-}
-
-export const createContest = async (contest) => {
-    var accessToken = await auth(username, password).then(data => data.accessToken)
-
-    const newContest = {
-        ...contest,
-        creatorId: '618dcea8a611f4340296328c',
-        isHidden: false,
-        createdAt: Date.now(),
-        updatedAt: Date.now()
-    }
-
-    const url = `${API_ROOT}/app/api/v1/contests`
-    const request = await axios.post(url, newContest, {
         headers: {
             'Authorization': `Bearer ${accessToken}`
         }
@@ -53,9 +14,34 @@ export const createContest = async (contest) => {
     return request.data
 }
 
-export const updateContest = async (contest) => {
-    var accessToken = await auth(username, password).then(data => data.accessToken)
+export const getAllContestsByCreator = async (accessToken, creatorId) => {
+    //TODO: getAllContestsByCreator
+    const url = `${API_ROOT}/app/api/v1/contests/creator/${creatorId}`
+    const request = await axios.get(url, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    })
 
+    console.log(request.data)
+    return request.data
+}
+
+export const createContest = async (accessToken, contest) => {
+    //TODO: createContest
+    const url = `${API_ROOT}/app/api/v1/contests`
+    const request = await axios.post(url, contest, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    })
+
+    console.log(request.data)
+    return request.data
+}
+
+export const updateContest = async (accessToken, contest) => {
+    //TODO: updateContest
     const url = `${API_ROOT}/app/api/v1/contests/${contest.id}`
     const request = await axios.put(url, contest, {
         headers: {
@@ -67,10 +53,8 @@ export const updateContest = async (contest) => {
     return request.data
 }
 
-export const updateTestsInContest = async (contestId, testsList) => {
+export const updateTestsInContest = async (accessToken, contestId, testsList) => {
     //TODO: update các bài tests trong contest
-    var accessToken = await auth(username, password).then(data => data.accessToken)
-
     const url = `${API_ROOT}/app/api/v1/contests/${contestId}/tests`
     const request = await axios.put(url, testsList, {
         headers: {
@@ -82,9 +66,8 @@ export const updateTestsInContest = async (contestId, testsList) => {
     return request.data
 }
 
-export const deleteContest = async (contest) => {
-    var accessToken = await auth(username, password).then(data => data.accessToken)
-
+export const deleteContest = async (accessToken, contest) => {
+    //TODO: deleteContest
     const url = `${API_ROOT}/app/api/v1/contests/${contest.id}/delete`
     const request = await axios.post(url, contest, {
         headers: {
