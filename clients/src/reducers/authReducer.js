8741b56119ade03ee152a45fdcb439dc885e710f
timothy tran <tadeothien@gmail.com>
2021-12-03 13:51:54 +0700

    login fail ((

---
+++ clients/src/reducers/authReducer.js
@@ -0,0 +1,20 @@
+const authReducer = (state, action) => {
+    const {
+        type,
+        payload: { isAuthenticated, user }
+    } = action
+
+    switch (type) {
+    case 'SET_AUTH':
+        return {
+            ...state,
+            authLoading: false,
+            isAuthenticated,
+            user
+        }
+    default:
+        return state
+    }
+}
+
+export default authReducer
