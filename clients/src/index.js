fd4e112ece3261fbeb6b08840890da05758a42f7
NguyenHoang292211 <ngungungu313131@gmail.com>
2021-12-04 01:47:47 +0700

    create redux

--- clients/src/index.js
+++ clients/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./app/store";
 ReactDOM.render(
+  <Provider store={store}>
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
 
-reportWebVitals()
+reportWebVitals();
