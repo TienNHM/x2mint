87aba7b7b397d575b19ead87c3a9f41bcd432950
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-01-01 11:03:36 +0700

    refactor code

--- clients/src/pages/home/Homepage.js
+++ clients/src/pages/home/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Image } from 'react-bootstrap'
 import './Homepage.scss'
-import Footer from 'components/common/home/Footer'
+import Footer from 'pages/home/Footer'
 
 
 function Homepage() {
