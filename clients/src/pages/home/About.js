375be6f4b60c19e541c154c02dde06d4fa4dbc11
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-01-01 22:23:37 +0700

    edit content in homepage

--- clients/src/pages/home/About.js
+++ clients/src/pages/home/About.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Image } from 'react-bootstrap'
+import { Badge, Button, Image } from 'react-bootstrap'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import { Link } from 'react-router-dom'
 
 AOS.init({
     duration: 500
@@ -17,24 +18,36 @@ export default function About() {
     return (
         <div className="about container-fluid" id="about"
             style={styleTop}>
-            <div data-aos="fade-up">
+            <div data-aos="fade-up" className="pb-3 mb-3">
                 <Image src={process.env.PUBLIC_URL + '/assets/icons/plant-line.svg'}></Image>
                 <h2 className='text-center'>
                     Thuận tiện - Nhanh chóng - Hiệu quả - Bảo mật
                 </h2>
             </div>
-            <div className="row w-100 p-3 d-flex justify-content-center align-items-center">
-                <div className="col-12 col-md-6" data-aos="fade-up">
-                    <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+
+            <div className="row container w-100 p-3 d-flex justify-content-center align-items-center">
+                <div className="col-12 col-md-6" data-aos="fade-up"
+                    style={{ textAlign: 'justify' }}>
+                    <h5>
+                        Chỉ cần đăng ký tài khoản, bạn đã có thể tham gia vào các cuộc thi do các đơn vị tổ chức trực tuyến trên nền tảng <Badge pill bg="success" className="mx-1">X2M!NT</Badge> một cách nhanh chóng!
+                    </h5>
+                    <h5>
+                        Khi tham gia thi, người dự thi sẽ được làm bài trong thời gian quy định của bài thi. Đồng thời, trong suốt quá trình thi, mọi hoạt động của thí sinh trên hệ thống đều được ghi nhận lại.
+                    </h5>
+                    <h5>
+                        Để đảm bảo tính an toàn, bảo mật và khách quan, hệ thống sẽ ngăn chặn cách hành vi <Badge pill bg="warning" className="mx-1">Chuyển tab</Badge> <Badge pill bg="danger" className="mx-1">Thoát toàn màn hình</Badge>.
+                        Thí sinh nếu vi phạm vượt quá số lần quy định thì bài thi sẽ được hệ thống đóng lại.
+                    </h5>
                 </div>
 
-                <div className="col-12 col-md-6" data-aos="fade-up">
+                <div className="col-12 col-md-6 con" data-aos="fade-up">
                     <Image className="w-50"
                         src={process.env.PUBLIC_URL + 'assets/contest.svg'} />
                     <p id="create">DỰ THI</p>
                 </div>
             </div>
-            <div className="row w-100 p-3 d-flex justify-content-center align-items-center">
+
+            <div className="row container w-100 p-3 d-flex justify-content-center align-items-center">
                 <div className="col-12 col-md-6" data-aos="fade-up">
                     <Image className="w-50"
                         src={process.env.PUBLIC_URL + '/assets/create.svg'} />
@@ -42,8 +55,42 @@ export default function About() {
                 </div>
 
                 <div className="col-12 col-md-6" data-aos="fade-up">
-                    <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+                    <h5 className="text-justify d-flex align-items-center flex-column">
+                        Người tổ chức cuộc thi có thể tạo các cuộc thi với đa dạng các tính năng:
+                        <div className="d-flex align-items-start flex-column">
+                            <li className="my-1">
+                                <Badge pill bg="success" className="mx-1">Tùy chỉnh thời gian</Badge> diễn ra cuộc thi
+                            </li>
+                            <li className="my-1">
+                                <Badge pill bg="success" className="mx-1">Kho ảnh minh họa</Badge> phong phú, đa dạng
+                            </li>
+                            <li className="my-1">
+                                <Badge pill bg="success" className="mx-1">Import đề thi</Badge> từ file Excel
+                            </li>
+                            <li className="my-1">
+                                <Badge pill bg="success" className="mx-1">Xem thống kê</Badge> chi tiết về cuộc thi
+                            </li>
+                            <li className="my-1">
+                                <Badge pill bg="success" className="mx-1">Xuất file Excel</Badge> kết quả chi tiết
+                            </li>
+                        </div>
+                    </h5>
                 </div>
+            </div>
+
+            <br/>
+            <div className="row w-50 mt-3 p-3 d-flex justify-content-center align-items-center">
+                <h2 className='text-center'>
+                    Còn chần chừ gì mà không tham gia ngay nào!
+                </h2>
+                <Image src={process.env.PUBLIC_URL + '/assets/svg/login.svg'} />
+                <Link to="/register">
+                    <Button variant="success"
+                        className="fw-bolder" pill>
+                        <i className="fa fa-graduation-cap pe-1"></i>
+                        Đăng ký
+                    </Button>
+                </Link>
             </div>
         </div>
     )
