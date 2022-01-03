dc54496c6d28500e0981f26c2cae7c9aed16a98d
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-01-03 16:06:02 +0700

    fix useParams, useNavigate

--- clients/src/components/account/register/ActivationEmail.js
+++ clients/src/components/account/register/ActivationEmail.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useNavigate } from 'react-router'
+import { useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 const ActivationEmail = () => {
