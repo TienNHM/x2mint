66921f560af61a6e04c842b47ad2155eba1dca83
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-07-09 02:12:23 +0700

    fix layout for `Test`

--- clients/src/components/exam/panelSettings/ModalTestInfo.js
+++ clients/src/components/exam/panelSettings/ModalTestInfo.js
@@ -6,10 +6,12 @@ import { displayTimeDelta, splitTime } from 'utils/timeUtils'
 export default function ModalTestInfo({ isShow, onAction, test, isUser }) {
     const start = splitTime(test.startTime)
     const end = splitTime(test.endTime)
+    const testUrl = process.env.REACT_APP_WEBSITE + test.url
 
     const [testName, setTestName] = useState(test.name)
     const [maxPoints, setMaxPoints] = useState(test.maxPoints)
-    const [link, setLink] = useState(test.url)
+    const [testNumberOfTimes, setTestNumberOfTimes] = useState(test.maxTimes !== null ? test.maxTimes : 1)
+    const [link, setLink] = useState(testUrl)
     const [pin, setPin] = useState(test.pin ? test.pin : '')
     const [description, setDescription] = useState(test.description)
     const [startDate, setStartDate] = useState(start.date)
@@ -21,6 +23,7 @@ export default function ModalTestInfo({ isShow, onAction, test, isUser }) {
         const newTest = {
             name: testName,
             maxPoints: maxPoints,
+            maxTimes: testNumberOfTimes,
             url: link,
             pin: pin,
             description: description,
@@ -41,47 +44,72 @@ export default function ModalTestInfo({ isShow, onAction, test, isUser }) {
             keyboard={false}>
 
             <Modal.Header closeButton className="d-flex justify-content-center">
-                <Modal.Title className="h5 fw-bolder">Thông tin chi tiết</Modal.Title>
+                <Modal.Title className="h5 fw-bolder">{testName || 'Tạo mới bài kiểm tra'}</Modal.Title>
             </Modal.Header>
 
             <Modal.Body>
                 <div className="row">
                     <div className="col-sm-6 col-xs-12">
-                        <div className="test-title-section">
-                            <div className="label fw-bold">Tên</div>
-                            <Form.Control
-                                size="sm"
-                                type="text"
-                                className="test-title"
-                                value={testName}
-                                onChange={(e) => setTestName(e.target.value)}
-                                readOnly={isUser}
-                            />
-                        </div>
+                        {!isUser &&
+                            <div className="test-title-section">
+                                <div className="label fw-bold">Tên</div>
+                                <Form.Control
+                                    size="sm"
+                                    type="text"
+                                    className="test-title"
+                                    value={testName}
+                                    onChange={(e) => setTestName(e.target.value)}
+                                    readOnly={isUser}
+                                />
+                            </div>
+                        }
 
-                        <div className="test-title-section mt-2">
-                            <div className="label fw-bold">Link</div>
-                            <Form.Control
-                                size="sm"
-                                type="text"
-                                className="test-title text-center"
-                                value={link}
-                                onChange={(e) => setLink(e.target.value)}
-                                readOnly={isUser}
-                            />
-                        </div>
+                        {isUser &&
+                            <div className="test-title-section mt-2">
+                                <div className="label fw-bold">Link</div>
+                                <Form.Control
+                                    size="sm"
+                                    type="text"
+                                    className="test-title"
+                                    value={link}
+                                    onChange={(e) => setLink(e.target.value)}
+                                    readOnly={true}
+                                />
+                            </div>
+                        }
 
                         <div className="test-title-section mt-2">
                             <div className="label fw-bold">Điểm tối đa</div>
                             <Form.Control
                                 size="sm"
                                 type="text"
-                                className="test-title text-center"
+                                className="test-title"
                                 value={maxPoints}
                                 onChange={(e) => setMaxPoints(e.target.value)}
                                 readOnly={isUser}
                             />
                         </div>
+
+                        {!isUser &&
+                            <div className="test-title-section mt-2">
+                                <div className="label fw-bold">
+                                    <abbr title="Đặt giá trị 0 nếu không giới hạn">
+                                        Số lượt làm bài
+                                    </abbr>
+                                </div>
+                                <div className="title">
+                                    <Form.Control
+                                        size="sm"
+                                        type="number"
+                                        min="0"
+                                        className="attribute-title-input"
+                                        value={testNumberOfTimes}
+                                        onChange={e => setTestNumberOfTimes(e.target.value)}
+                                        readOnly={isUser}
+                                    />
+                                </div>
+                            </div>
+                        }
                     </div>
 
                     <div className="col-sm-6 col-xs-12">
@@ -91,7 +119,7 @@ export default function ModalTestInfo({ isShow, onAction, test, isUser }) {
                                 <Form.Control
                                     size="sm"
                                     type="text"
-                                    className="test-title text-center"
+                                    className="test-title"
                                     value={pin}
                                     onChange={(e) => setPin(e.target.value)}
                                     readOnly={isUser}
@@ -105,7 +133,7 @@ export default function ModalTestInfo({ isShow, onAction, test, isUser }) {
                                 <Form.Control
                                     size="sm"
                                     type="text"
-                                    className="test-title text-center"
+                                    className="test-title"
                                     value={displayTimeDelta(test.startTime, test.endTime)}
                                     readOnly
                                 />
@@ -163,7 +191,7 @@ export default function ModalTestInfo({ isShow, onAction, test, isUser }) {
                     <Form.Control
                         size="sm"
                         as="textarea"
-                        rows="15"
+                        rows="12"
                         className="test-description"
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
@@ -173,17 +201,23 @@ export default function ModalTestInfo({ isShow, onAction, test, isUser }) {
             </Modal.Body>
 
             <Modal.Footer className="d-flex justify-content-center">
-                <Button variant="secondary"
-                    onClick={() => handleAction(MODAL_ACTION.CLOSE)}>
-                    Đóng
-                </Button>
+                {isUser && <>
+                    <Button variant="primary"
+                        onClick={() => handleAction(MODAL_ACTION.CONFIRM)}>
+                        Vào thi
+                    </Button>
+                </>}
 
-                {!isUser &&
+                {!isUser && <>
+                    <Button variant="secondary"
+                        onClick={() => handleAction(MODAL_ACTION.CLOSE)}>
+                        Đóng
+                    </Button>
                     <Button variant="primary"
                         onClick={() => handleAction(MODAL_ACTION.CONFIRM)}>
                         Lưu
                     </Button>
-                }
+                </>}
             </Modal.Footer>
         </Modal>
     )
